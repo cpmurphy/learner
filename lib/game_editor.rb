4f359da24a307a904876c6d606afc5d16951bf4d
Christian Murphy <cpmmu@yahoo.com>
2025-06-15 13:36:57 -0500

    chore: address rubocop complaints about GameEditor

--- lib/game_editor.rb
+++ lib/game_editor.rb
@@ -6,23 +6,28 @@ module GameEditor
   # it is moved to move M+1, as semantically $201 applies to the *next* move.
   # The PGN parser might associate it with M. This method corrects that.
   def self.shift_critical_annotations(game)
-    return unless game&.moves && game.moves.size > 1
+    moves = game.moves
+    i = moves.size - 1
+    while i.positive?
+      current_move = moves[i]
+      prev_move = moves[i - 1]
 
-    # Iterate in reverse to avoid issues with modifying the array during iteration.
-    (game.moves.size - 2).downto(0) do |i| # Iterate from second-to-last down to first move
-      current_move_obj = game.moves[i]
-      next_move_obj = game.moves[i + 1]
+      if prev_move.annotation&.include?('$201')
+        remove_201_from_move(prev_move)
 
-      next unless current_move_obj.annotation&.include?('$201')
-
-      # Remove $201 from the current move
-      current_move_obj.annotation.delete('$201')
-      # Clean up annotation array if it becomes empty
-      current_move_obj.annotation = nil if current_move_obj.annotation.empty?
-
-      # Add $201 to the next move
-      next_move_obj.annotation ||= [] # Initialize if nil
-      next_move_obj.annotation << '$201' unless next_move_obj.annotation.include?('$201')
+        add_201_to_move(current_move)
+      end
+      i -= 1
     end
+  end
+
+  def self.remove_201_from_move(move)
+    move.annotation.delete('$201')
+    move.annotation = nil if move.annotation.empty?
+  end
+
+  def self.add_201_to_move(move)
+    move.annotation ||= [] # Initialize if nil
+    move.annotation << '$201' unless move.annotation.include?('$201')
   end
 end
